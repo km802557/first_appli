74131a98171941a7080986219efb29fdb0bb17ee
trollon <kueny.mickael@etu.unice.fr>
2012-10-11 12:25:40 +0200

    Premier import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstAppli::Application.config.secret_token = '0b2726ba7facc6760ef0fc74dc2b70a567ab973f690ae6ead169c518d72a5ec7a40c56917edc1b0ef8bb53da673ab192c558e97585f93e5fb9df108d41d1c1ea'
